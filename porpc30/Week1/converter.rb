d9a57b54f9096ea2f43e67aceb903ab0dfe9cbd0
Robie Lutsey <robie1373@gmail.com>
2012-01-28 23:30:49 -0500

    fixored an error and prettied up exceptions

--- porpc30/Week1/converter.rb
+++ porpc30/Week1/converter.rb
@@ -1,18 +1,30 @@
 def convert(temp, unit)
-  if unit == 'C'
-    @out_temp = temp * 9.0 / 5.0 + 32.0
-    @out_unit = 'F'
-  elsif @out_temp = temp *5.0 / 9.0 - 32.0
-    @out_unit = 'C'
+  if unit.upcase == 'C'
+    if temp >= -273.15
+      @out_temp = (temp * 9.0 / 5.0) + 32.0
+      @out_unit = 'F'
+    else
+      @error_code += 1
+      raise "below absolute zero."
+    end
+  elsif if unit.upcase == 'F'
+          if temp >= -459.67
+            @out_temp = (temp -32.0) * 5.0 / 9.0
+            @out_unit = 'C'
+          else
+            @error_code +=1
+            raise "below Absolute zero."
+          end
+        end
   else
-    raise Exception "bad input"
+    raise
   end
 end
 
 def parse(input)
-  args  = input.split
-  @temp = args[0].to_f || 20.0
-  @unit = args[1].upcase! || 'C'
+  @args = input.split(' ')
+  @temp = @args[0].to_f || 20.0
+  @unit = @args[1] #|| 'C'
 end
 
 def spiel
@@ -24,24 +36,28 @@ def spiel
 end
 
 spiel
-while input = gets
-
+while @input = gets
+  @error_code = 0
   begin
-    parse(input)
+    parse(@input)
   rescue Exception
+    @error_code += 1
     puts "I don't understand the input. You're probably either missing the unit or the space between the number and the unit"
     sleep 1
     puts "\nWhile you work that out, did you know:"
   end
 
   begin
-    convert(@temp, @unit)
+    junk = convert(@temp, @unit)
   rescue Exception
-    puts "\nUggh. Math is hard. #{$!}\n"
+    puts "\nUggh. Math is hard #{$!}\n"
+    @error_code += 1
     sleep 1
   end
 
-  puts "#{@temp} #{@unit} is #{@out_temp} #{@out_unit}."
+  if @error_code == 0
+    puts "#{@temp} #{@unit} is #{@out_temp} #{@out_unit}."
+  end
   spiel
 
 end
